be14bc89d4930c4e72d68d169c2b71fae3f92303
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-06-12 10:33:09 +0300

    feature: bind arguemnt to function

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-module.exports.tryCatch = function () {
-    console.log("Hello there")
+const log = console.log;
+
+const tryCatchWrapper = function (func) {
+  try {
+    func();
+  } catch (error) {
+    console.log("Error happened");
+  }
 }
+
+const tryCatch = function (func, arg) {
+  return tryCatchWrapper(func.bind(this, arg))
+}
+
+function test(a) {
+  console.log({a})
+  console.log(window.mm.a);
+}
+
+tryCatch(test.bind(this, 1));
