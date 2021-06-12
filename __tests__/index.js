554f988711103f5f6c84f0c6d7e1feae851aaba9
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-06-12 11:55:12 +0300

    test: function call

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,5 +1,12 @@
 const { tryCatch } = require("../index");
 
+const functionToBeCalled = jest.fn();
+test("It calls the function passed to it with correct arguments", () => {
+  const args = [1, 2, 3, { a: "" }, [], null];
+  tryCatch(functionToBeCalled, ...args);
+  expect(functionToBeCalled).toHaveBeenCalledWith(...args);
+});
+
 function functionThatWillError(num) {
   /** I am calling a number */
   num();
