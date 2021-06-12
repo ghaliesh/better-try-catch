ba0a5bfc80f721b43ccd57370e3dbe2f8d2ac260
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-06-12 15:17:25 +0300

    feature: support asycn functions

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ const tryCatchWrapper = function (func) {
   } catch (error) {
     return [null, error];
   }
-}
+};
+
+const asynsCatchWrapper = async function (func) {
+  try {
+    const result = await func();
+    return [result, null];
+  } catch (error) {
+    return [null, error];
+  }
+};
 
 module.exports.tryCatch = function (func, ...args) {
-  return tryCatchWrapper(func.bind(this, ...args))
-}
+  return tryCatchWrapper(func.bind(this, ...args));
+};
+
+module.exports.asyncTryCatch = async function (func, ...args) {
+  return await asynsCatchWrapper(func.bind(this, ...args));
+};
